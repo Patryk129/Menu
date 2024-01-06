174e212e35418071fc505783f63955d03da7467b
Patryk129 <112767162+Patryk129@users.noreply.github.com>
2024-01-06 17:11:26 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int Menu()
     cout<<"1. Dodaj element tablicy"<<endl;
     cout<<"2. Wstaw element tablicy"<<endl;
     cout<<"3. Usun element tablicy"<<endl;
-    cout<<"4. WyÅ›wietl elementy tablicy"<<endl;
+    cout<<"4. Wyœwietl elementy tablicy"<<endl;
     cout<<"5. Zapisz do pliku"<<endl;
     cout<<"6. Wczytaj z pliku"<<endl;
     cout<<"7. Wyjscie z programu"<<endl;
@@ -35,21 +35,21 @@ int main()
         case 1:
             {
                 int * tmp = NULL;
-                tmp = new int[n+1]; //tworzenie nowej tablicy (o jeden wiÄ™kszej niÅ¼ poprzednia)
-                if(p != NULL) //jeÅ¼eli istnieje poprzednia tablica to:
+                tmp = new int[n+1]; //tworzenie nowej tablicy (o jeden wiêkszej ni¿ poprzednia)
+                if(p != NULL) //je¿eli istnieje poprzednia tablica to:
                 {
                     for(int i=0;i<n;i++)
-                    {//przekopiuj zawartoÅ›Ä‡ poprzedniej tablicy do nowej
+                    {//przekopiuj zawartoœæ poprzedniej tablicy do nowej
                         tmp[i] = p[i];
                     }
-                    //usuÅ„ poprzedniÄ… tablicÄ™
+                    //usuñ poprzedni¹ tablicê
                     delete[] p;
 
                 }
                 cout<<"Podaj wartosc elementu tablicy: ";
-                cin>>tmp[n];  //wstaw nowÄ… wartoÅ›Ä‡ do tablicy (na koniec)
-                p = tmp; //przekopiuj adres nowej tablicy do wskaÅºnika p
-                n++; //zwiÄ™ksz iloÅ›Ä‡ elementÃ³w tablicy o 1
+                cin>>tmp[n];  //wstaw now¹ wartoœæ do tablicy (na koniec)
+                p = tmp; //przekopiuj adres nowej tablicy do wskaŸnika p
+                n++; //zwiêksz iloœæ elementów tablicy o 1
                 break;
             }
         case 2:
@@ -97,40 +97,94 @@ int main()
                 break;
             }
         case 3:
-            {
-                int miejsce;
-                cout<<"Podaj miejsce usuwanego elementu: ";
-                cin>>miejsce;
-                int* tmp = NULL;
-                tmp = new int[n-1];
-                if(p != NULL)
+            {//nie dziala jeszcze
+            /*
+                if(n==0)
                 {
-                    for(int i=0;i<n;i++)
+                    cout<<"Tablica jest pusta."<<endl;
+                }
+                else
+                {
+                    int miejsce;
+                    cout<<"Podaj miejsce usuwanego elementu: ";
+                    cin>>miejsce;
+                    int* tmp = NULL;
+                    tmp = new int[n];
+                    if(p != NULL)
                     {
-                        if(i==miejsce)
+                        for(int i=0;i<n;i++)
                         {
-                            tmp[i] = p[i+1];
+                            if(i==miejsce)
+                            {
+                                tmp[i] = p[i+1];
+                            }
+                            else if(i<miejsce)
+                                tmp[i] = p[i];
+                            else if(i<n-1)
+                                tmp[i]=0;
+                            else
+                                tmp[i] = p[i+1];
                         }
-                        else if(i<miejsce)
-                            tmp[i] = p[i+1];
-                        else
-                            tmp[i] = p[i+1];
+                        delete[] p;
+                    }
+                    p = tmp;
+                    n--;
+                }
+
+                break;
+                */
+                 if(n==0)
+                {
+                    cout<<"Tablica jest pusta."<<endl;
+                    break;
+                }
+                else
+                {
+                    int miejsce = 0;
+                    if(n!=1)
+                    {
+                        cout<<"Podaj miejsce usuwanego elementu: ";
+                        cin>>miejsce;
+                    }
+                    int* tmp = NULL;
+                    cout<<n;
+                    if(p != NULL)
+                    {
+                        for(int i=0;i<n-1;i++)
+                        {
+                            if(i >= miejsce)
+                            {
+                               tmp[i] = p[i+1];
+                            }
+                            else
+                            {
+                               tmp[i] = p[i];
+                            }
+                            cout<<tmp[i]<<endl;
+                        }
                     }
                     delete[] p;
+                    p = tmp;
+                    n--;
+                    break;
                 }
-                p = tmp;
-                n--;
-                break;
             }
         case 4:
             {
-                cout<<"Zawartosc tablicy:"<<endl;
-                for(int i=0;i<n-1;i++)
+                if(n==0)
                 {
-                    cout<<p[i]<<", ";
+                    cout<<"Tablica jest pusta."<<endl;
                 }
-                cout<<p[n-1]<<".";
-                cout<<endl;
+                else
+                {
+                    cout<<"Zawartosc tablicy:"<<endl;
+                    for(int i=0;i<n-1;i++)
+                    {
+                        cout<<p[i]<<", ";
+                    }
+                    cout<<p[n-1]<<"."<<endl;;
+                    cout<<"Tablica ma "<<n<<" elementow."<<endl;
+                }
                 break;
             }
         case 5:
